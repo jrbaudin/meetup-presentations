c63c4f410bea40fbd950c185fd6189f089d1414d
Joel Baudin <joel.baudin88@gmail.com>
2018-06-27 11:03:22 +0200

    Removed dummy slides

--- packages/server-side-graphql/presentation/index.js
+++ packages/server-side-graphql/presentation/index.js
@@ -199,22 +199,10 @@ export default class Presentation extends React.Component {
           />
         </Slide>
         <Slide transition={["fade"]} bgColor="tertiary">
-          <Heading size={6} textColor="primary" caps>Typography</Heading>
-          <Heading size={1} textColor="secondary">Heading 1</Heading>
-          <Heading size={2} textColor="secondary">Heading 2</Heading>
-          <Heading size={3} textColor="secondary">Heading 3</Heading>
-          <Heading size={4} textColor="secondary">Heading 4</Heading>
-          <Heading size={5} textColor="secondary">Heading 5</Heading>
-          <Text size={6} textColor="secondary">Standard text</Text>
+          <Heading size={3} textColor="primary">Any questions?</Heading>
         </Slide>
         <Slide transition={["fade"]} bgColor="primary" textColor="tertiary">
-          <Heading size={6} textColor="secondary" caps>Standard List</Heading>
-          <List>
-            <ListItem>Item 1</ListItem>
-            <ListItem>Item 2</ListItem>
-            <ListItem>Item 3</ListItem>
-            <ListItem>Item 4</ListItem>
-          </List>
+          <Heading size={6} textColor="secondary" caps>DEMO</Heading>
         </Slide>
       </Deck>
     );
